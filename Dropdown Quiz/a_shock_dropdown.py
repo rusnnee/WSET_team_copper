91dd30e676f207ab933f6c521e073ffe4c69cd21
RUSNE JONEIKYTE <rusne.jon@gmail.com>
2022-03-05 22:39:22 +0000

    A-shock quiz formatting

--- Dropdown Quiz/a_shock_dropdown.py
+++ Dropdown Quiz/a_shock_dropdown.py
@@ -1,21 +1,22 @@
 # Import module
 from tkinter import *
+from tkinter import font
 import random
 import copy
 
 # Create object
 root = Tk()
-
-# Adjust size
 root.geometry( "1920x1080" )
+root.title('Anyphalactic Shock Process quiz')
+root.configure(bg='black')
 
 #Correct list
 correct_list = [
     "Call 999",
-    "Check if the person is carrying any medication",
-    "Help administer medication or, if trained, do it yourself",
-    "After injection, continue to look after the person until medical help arrives",
-    "Make sure the casualty is comfortable and can breathe as best as they can",
+##    "Check if the person is carrying any medication",
+##    "Help administer medication or, if trained, do it yourself",
+##    "After injection, continue to look after the person until medical help arrives",
+##    "Make sure the casualty is comfortable and can breathe as best as they can",
     ]
 
 # Button function
@@ -25,15 +26,24 @@ def show():
         text = clickers[n].get()
         if (text == correct_list[n]):
             correct += 1
-            drops[n].config(bg="GREEN", fg="WHITE")
+            drops[n].configure(state='disabled', disabledforeground = 'white')
+            drops[n].config(bg="GREEN", fg="WHITE" )
+            
+
         
-    label.config( text =  'The number of correct answers is ' + str(correct) )
+    if correct == len (correct_list):
+        label.config( text =  'Congratulations!' )
+
+#Title label
+title = Label( root , text = "How to help during Anaphylactic Shock?", bg = "black", fg = 'white' )
+title.config(font=('Comic Sans MS', '30'))
+title.pack(pady = 30)
 
 # Dropdown menu options
 options = copy.deepcopy(correct_list)
 random.shuffle(options)
 
-#MY OWN CODE
+#Dropsdown menu setup
 clickers = []
 drops = []
 
@@ -43,17 +53,22 @@ for n in range (len(correct_list)):
     clicked.set( "Task" + str(n+1))
     
     drop = OptionMenu( root , clicked , *options )
-    drop.pack()
+    drop.config (font=('Comic Sans MS', '15'), width = 80)
+    drop.pack(pady = 10)
+
+    menu = root.nametowidget(drop.menuname)
+    menu.config(font=('Comic Sans MS', '12'))
+
     clickers.append (clicked)
     drops.append (drop)
 
 
 
-# Create button, it will change label text
-button = Button( root , text = "Check" , command = show ).pack()
+# Button, it will check the order
+button = Button( root , text = "Check" , command = show,  font=('Comic Sans MS', '15') ).pack()
 
-# Create Label
-label = Label( root , text = " " )
+# Congradulations set up
+label = Label( root , text = "", bg = "black", fg = 'yellow', font=('Comic Sans MS', '25') )
 label.pack()
 
 # Execute tkinter
